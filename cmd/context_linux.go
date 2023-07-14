3b935020af93d6e4c8c35d1839ea1b9d67851485
Michael Dockter <docktermj@users.noreply.github.com>
2023-07-14 12:41:18 -0400

    Issue 13.dockter.1 (#16)
    * #13 Migrate to new method
    
    * #13 Migrate to new method

---
+++ cmd/context_linux.go
@@ -0,0 +1,15 @@
+//go:build linux
+
+package cmd
+
+import (
+	"github.com/senzing/senzing-tools/cmdhelper"
+)
+
+var ContextBoolsForOsArch = []cmdhelper.ContextBool{}
+
+var ContextIntsForForOsArch = []cmdhelper.ContextInt{}
+
+var ContextStringsForOsArch = []cmdhelper.ContextString{}
+
+var ContextStringSlicesForOsArch = []cmdhelper.ContextStringSlice{}
