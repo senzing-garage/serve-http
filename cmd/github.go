94f125883f48766f79b66f54f96b9b6f46ce582a
docktermj <docktermj@users.noreply.github.com>
2023-07-26 22:05:49 +0000

    make-go-github-file.yaml updated cmd/github.go for versioned release: 0.0.4

--- cmd/github.go
+++ cmd/github.go
@@ -1,11 +1,13 @@
-// 0.0.2
-// Created by make-go-github-file.yaml on Mon Jun 19 14:36:21 UTC 2023
+// 0.0.4
+// Created by make-go-github-file.yaml on Wed Jul 26 22:05:49 UTC 2023
 package cmd
 
-var githubDate string = "2023-07-21"
-var githubIteration string = "0"
-var githubRef string = "refs/tags/0.0.3"
-var githubRefName string = "0.0.3"
-var githubRepository string = "Senzing/serve-http"
-var githubRepositoryName string = "serve-http"
-var githubVersion string = "0.0.3"
+var (
+	githubDate           string = "2023-07-26"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.0.4"
+	githubRefName        string = "0.0.4"
+	githubRepository     string = "Senzing/serve-http"
+	githubRepositoryName string = "serve-http"
+	githubVersion        string = "0.0.4"
+)
