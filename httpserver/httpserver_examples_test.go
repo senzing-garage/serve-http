379e97d30a87d96dd5159f71984721aeb07f7ed4
Michael Dockter <michael@dockter.com>
2025-05-14 16:58:47 -0400

    #170 Working lint/test

--- httpserver/httpserver_examples_test.go
+++ httpserver/httpserver_examples_test.go
@@ -1,7 +1,12 @@
-package httpserver
+package httpserver_test
+
+import "github.com/senzing-garage/serve-http/httpserver"
 
 // ----------------------------------------------------------------------------
 // Examples for godoc documentation
 // ----------------------------------------------------------------------------
 
-func ExampleBasicHTTPServer_Serve() {}
+func ExampleBasicHTTPServer_Serve() {
+	_ = httpserver.BasicHTTPServer{}
+	// Output:
+}
